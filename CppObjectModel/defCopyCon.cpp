a417405e1bf207786e6f87abacbcc1cddbf75b42
xinstong <xinstong@gmail.com>
2018-03-20 22:43:24 -0700

    call explicit copy constructor.

--- CppObjectModel/defCopyCon.cpp
+++ CppObjectModel/defCopyCon.cpp
@@ -4,6 +4,17 @@ using namespace std;
 class A
 {
 public:
+    A()
+    {
+        cout << "A()" << endl;
+    }
+
+    A(const A& oa)
+    {
+        a = oa.a;
+        cout << "A(A oa)" << endl;
+    }
+
     int a;
 };
 
@@ -13,10 +24,21 @@ public:
     int b;
 };
 
+void fun(A ina)
+{
+    
+}
+
+
 int main()
 {
+#if 0
     B objb;
     objb.b = 2;
     A a(objb);
     cout << a.a << endl;
+#endif
+    A objA;
+    fun(objA);
+
 }
