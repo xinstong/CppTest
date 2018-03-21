14353625b99d33f953f121cda3a75062522f416f
xinstong <xinstong@gmail.com>
2018-03-20 22:25:45 -0700

    this code can't build in vs build.

---
+++ CppObjectModel/defCopyCon.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    int a;
+};
+
+class B : public A
+{
+public:
+    int b;
+};
+
+int main()
+{
+    B objb;
+    objb.b = 2;
+    A a(objb);
+}
